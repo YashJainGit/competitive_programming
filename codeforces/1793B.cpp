f655243a43659ba5c56e1fa2259961f1beeb7205
Yash Jain <itisyashjain@gmail.com>
2023-08-19 01:08:26 -0700

    1793B

---
+++ codeforces/1793B.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+#define llmax LLONG_MAX;
+#define llmin LLONG_MIN;
+#define mod 1000000007;
+
+#define rep(x,y,z) for(int x=(y);x<=(z);x++)
+
+const bool testcases = true;
+void solve(){
+    ll y, x;
+    cin >> y >> x;
+
+    cout << (y-x)*2 << '\n';
+    for (ll i=y; i>x ; i--){
+        cout << i << ' ';
+    }
+    for (ll i=x; i<y; i++){
+        cout << i << ' ';
+    }
+    cout << '\n';
+
+}
+
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout<<fixed<<setprecision(0);
+    ll t=1;
+    if (testcases) cin >> t;
+    while(t--) solve();
+    return 0;
+}
+
