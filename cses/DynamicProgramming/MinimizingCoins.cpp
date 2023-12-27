1c2ce707698f5e2a30d050c5e9d7eb136f92407e
Yash Jain <ItIsYashJain@gmail.com>
2023-12-26 18:33:36 -0800

    Learning dp

--- cses/DynamicProgramming/MinimizingCoins.cpp
+++ cses/DynamicProgramming/MinimizingCoins.cpp
@@ -2,8 +2,50 @@
 
 using namespace std;
 
+vector<int> c;
+vector<int> dp(1e6+1);
+
+int trav(int n){
+    if (n==0){
+        return 1;
+    }
+    if (n<0){
+        return INT_MAX;
+    }
+    if (dp[n]!=-1){
+        return dp[n];
+    }
+
+    int mn=INT_MAX;
+    for (int i:c){
+        mn = min(mn, trav(n-i));
+    }
+    dp[n] = mn;
+    if (mn!=INT_MAX){
+        dp[n]++;
+    }
+    return dp[n];
+}
+
+
 void solve(){
-    
+    fill(dp.begin(), dp.end(), -1);
+    int n, x;
+    cin >> n >> x;    
+
+    c.resize(n);
+    for (int &i:c){
+        cin >> i;
+    }
+
+    int ans = trav(x);
+    if (ans==INT_MAX){
+        cout << -1;
+    }
+    else{
+        cout << ans-1;
+    }
+
     
 }
 
